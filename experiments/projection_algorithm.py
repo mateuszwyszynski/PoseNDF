d7a9dfea90431b5bce5aba84e1afbcb5eb3f173b
Mateusz <mat.wyszynski@gmail.com>
2024-02-24 18:31:23 +0100

    chore: renamed noisy_poses variable to simply poses
    All poses can be projected. There is no need to make such a distinction with the variable name

--- experiments/projection_algorithm.py
+++ experiments/projection_algorithm.py
@@ -30,42 +30,42 @@ class Projector(object):
         self.out_path = out_path
 
 
-    def projection_step(self, noisy_poses):
-        net_pred = self.pose_prior(noisy_poses, train=False)
-        grad_val = gradient(noisy_poses, net_pred['dist_pred']).reshape(-1, 84)
-        noisy_poses = noisy_poses.detach()
+    def projection_step(self, poses):
+        net_pred = self.pose_prior(poses, train=False)
+        grad_val = gradient(poses, net_pred['dist_pred']).reshape(-1, 84)
+        poses = poses.detach()
         net_pred['dist_pred'] = net_pred['dist_pred'].detach()
         print(torch.mean(net_pred['dist_pred']))
         grad_norm = torch.nn.functional.normalize(grad_val, p=2.0, dim=-1)
-        noisy_poses = noisy_poses - (net_pred['dist_pred']*grad_norm).reshape(-1, 21,4)
-        noisy_poses, _ = quat_flip(noisy_poses)
-        noisy_poses = torch.nn.functional.normalize(noisy_poses,dim=-1)
-        noisy_poses = noisy_poses.detach()
-        noisy_poses.requires_grad = True
+        poses = poses - (net_pred['dist_pred']*grad_norm).reshape(-1, 21,4)
+        poses, _ = quat_flip(poses)
+        poses = torch.nn.functional.normalize(poses,dim=-1)
+        poses = poses.detach()
+        poses.requires_grad = True
 
-        return noisy_poses
+        return poses
 
 
-    def project(self, noisy_poses, iterations=100, save_projection_steps=True):
-        noisy_poses, _ = quat_flip(noisy_poses)
-        noisy_poses = torch.nn.functional.normalize(noisy_poses,dim=-1)
+    def project(self, poses, iterations=100, save_projection_steps=True):
+        poses, _ = quat_flip(poses)
+        poses = torch.nn.functional.normalize(poses,dim=-1)
 
-        noisy_poses.requires_grad = True
+        poses.requires_grad = True
 
         if save_projection_steps:
             projection_steps_path = os.path.join(self.out_path, 'projection_steps')
             os.makedirs(projection_steps_path, exist_ok=True)
-            batch_size, _, _ = noisy_poses.shape
-            projection_steps = torch.detach(noisy_poses).reshape(batch_size, 1, -1, 4)
+            batch_size, _, _ = poses.shape
+            projection_steps = torch.detach(poses).reshape(batch_size, 1, -1, 4)
         
         for _ in range(iterations):
-            noisy_poses = self.projection_step(noisy_poses)
+            poses = self.projection_step(poses)
 
             if save_projection_steps:
-                projection_steps = torch.cat((projection_steps, noisy_poses.reshape(batch_size, 1, -1, 4)), dim=1)
+                projection_steps = torch.cat((projection_steps, poses.reshape(batch_size, 1, -1, 4)), dim=1)
 
         if save_projection_steps:
             for motion_ind in range(batch_size):
                 np.savez(os.path.join(projection_steps_path, f'{motion_ind}.npz'), pose_body=np.array(projection_steps[motion_ind].cpu().detach().numpy()))
 
-        return noisy_poses
+        return poses
